5b458a54b057b8ef1d74375712221ea27a6732cb
NealMann08 <nealmann08@gmail.com>
2025-06-11 15:27:15 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myGeoSecureApp',
+  webDir: 'dist'
+};
+
+export default config;
